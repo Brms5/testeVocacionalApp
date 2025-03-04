50e480ba7fe98c25c35d67478b5639068a3aaa7a
Brms5 <moreirasbr@gmail.com>
2025-03-04 18:16:00 -0300

    Comentando a função de autenticação.

--- src/server/infra/security/auth.ts
+++ src/server/infra/security/auth.ts
@@ -1,38 +1,37 @@
-import { Usuario } from "@/server/schema/Usuario";
-import jsonwebtoken from "jsonwebtoken";
+// import jsonwebtoken from "jsonwebtoken";
 
-function createToken(usuario: Usuario): string {
-    const token = jsonwebtoken.sign(
-        { id: usuario.id, email: usuario.email },
-        "secret",
-        {
-            expiresIn: "1h",
-        }
-    );
+// function createToken(usuario: Usuario): string {
+//     const token = jsonwebtoken.sign(
+//         { id: usuario.id, email: usuario.email },
+//         "secret",
+//         {
+//             expiresIn: "1h",
+//         }
+//     );
 
-    return token;
-}
+//     return token;
+// }
 
-function verifyToken(token: string | null): void {
-    // verify if the token exists
-    if (token === null) {
-        throw new Error("Token não encontrado.");
-    }
+// function verifyToken(token: string | null): void {
+//     // verify if the token exists
+//     if (token === null) {
+//         throw new Error("Token não encontrado.");
+//     }
 
-    // verify if the token is valid
-    jsonwebtoken.verify(token, "secret", (err: any) => {
-        if (err) {
-            throw new Error("Token inválido.");
-        }
-    });
-}
+//     // verify if the token is valid
+//     jsonwebtoken.verify(token, "secret", (err: any) => {
+//         if (err) {
+//             throw new Error("Token inválido.");
+//         }
+//     });
+// }
 
-interface InfraSecurity {
-    createToken: (usuario: Usuario) => string;
-    verifyToken: (token: string | null) => void;
-}
+// interface InfraSecurity {
+//     createToken: (usuario: Usuario) => string;
+//     verifyToken: (token: string | null) => void;
+// }
 
-export const infraSecurity: InfraSecurity = {
-    createToken,
-    verifyToken,
-};
+// export const infraSecurity: InfraSecurity = {
+//     createToken,
+//     verifyToken,
+// };
